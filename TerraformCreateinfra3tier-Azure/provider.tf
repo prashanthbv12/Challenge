88dcad33325eeb309796b7c193410d285da10eb5
Vcods <45381818+prashanthbv12@users.noreply.github.com>
2023-07-15 22:26:10 +0530

    Update provider.tf

--- TerraformCreateinfra3tier-Azure/provider.tf
+++ TerraformCreateinfra3tier-Azure/provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
-  subscription_id = "f555b75c-dd95-439c-b81c-7e37d9dc073d"
-  client_id       = "43c4512c-e795-4252-be87-dee278bda683"
-  tenant_id       = "c6d5705d-c3d8-44a2-9454-7f00b90ee67c"
-  client_secret   = "HU98Q~rhQNGxMB3um1WzDenb1djsgFD09l4gbc0a"
+  subscription_id = ""
+  client_id       = ""
+  tenant_id       = ""
+  client_secret   = ""
 }
